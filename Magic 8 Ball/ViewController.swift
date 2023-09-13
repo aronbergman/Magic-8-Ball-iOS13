17c61d8f55d67b8e8f75e981379e75b079ddffce
aronbergman <brgmn@icloud.com>
2023-09-13 14:01:00 +0200

    feat(view): added logic for random image selection

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,12 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     
-//    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
     @IBOutlet weak var imageView: UIImageView!
     
     @IBAction func askButtonPressed(_ sender: Any) {
-        imageView.image = #imageLiteral(resourceName: "ball2")
+        
+        imageView.image = ballArray[Int.random(in: 0...4)]
     }
     
 }
